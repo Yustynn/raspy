f72ac73d8e1003a3f1fa8df1f88c642eb6aae373
Yustynn Panicker <yustynn@gmail.com>
2016-07-26 14:33:36 +0800

    Run servo

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const { red, blue, yellow, green, orange } = require('chalk');
 
 console.log(green('bongiorno world'));
+
+const { Gpio } = require('pigpio'),
+  motor = new Gpio( 10, { mode:Gpio.OUTPUT } ),
+  pulsWidth = 100,
+  increment = 100;
+
+setInterval( () => {
+  motor.servoWrite(pulseWidth);
+
+  pulsWidth += increment;
+  pulseWidth += (pulseWidth >= 2000) ? -100 : 100;
+});
