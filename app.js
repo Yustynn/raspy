3e9ad41f16fe54f49e4f830a05a0036f615934b5
Yustynn Panicker <yustynn@gmail.com>
2016-07-27 22:46:17 +0800

    Trying new package

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const { red, blue, yellow, green, orange } = require('chalk');
+var gpio = require("gpio");
 
-console.log(green('bongiorno world'));
+// Calling export with a pin number will export that header and return a gpio header instance
+var gpio4 = gpio.export(4, {
+   // When you export a pin, the default direction is out. This allows you to set
+   // the pin value to either LOW or HIGH (3.3V) from your program.
+   direction: 'out',
 
-const { Gpio } = require('pigpio'),
-  motor = new Gpio( 3, { mode:Gpio.OUTPUT } ),
-  pulsWidth = 100,
-  increment = 100;
+   // set the time interval (ms) between each read when watching for value changes
+   // note: this is default to 100, setting value too low will cause high CPU usage
+   interval: 200,
 
-setInterval( () => {
-  motor.servoWrite(pulseWidth);
-
-  pulseWidth += increment;
-  pulseWidth += (pulseWidth >= 2000) ? -100 : 100;
+   // Due to the asynchronous nature of exporting a header, you may not be able to
+   // read or write to the header right away. Place your logic in this ready
+   // function to guarantee everything will get fired properly
+   ready: function() {
+   }
 });
