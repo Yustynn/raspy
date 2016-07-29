a2bf7f82a96dbf78f45fa23b95fe379c7e2a2938
Yustynn Panicker <yustynn@gmail.com>
2016-07-29 15:35:30 +0800

    Old code

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
-var gpio = require('rpi-gpio');
+var Gpio = require('pigpio').Gpio,
+  motor = new Gpio(10, {mode: Gpio.OUTPUT}),
+  pulseWidth = 1000,
+  increment = 100;
 
-var pin   = 4;
-var delay = 1000;
-var count = 0;
-var max   = 10;
+setInterval(function () {
+  motor.servoWrite(pulseWidth);
 
-gpio.setup(pin, gpio.DIR_OUT, on);
-
-function on() {
-  console.log('on called!');
-    if (count >= max) {
-        gpio.destroy(function() {
-            console.log('Closed pins, now exit');
-        });
-        return;
-    }
-
-    setTimeout(function() {
-        gpio.write(pin, 1, off);
-        count += 1;
-    }, delay);
-}
-
-function off() {
-  console.log('off called!');
-    setTimeout(function() {
-        gpio.write(pin, 0, on);
-    }, delay);
-}
+  pulseWidth += increment;
+  if (pulseWidth >= 2000) {
+    increment = -100;
+  } else if (pulseWidth <= 1000) {
+    increment = 100;
+  }
+}, 1000);
