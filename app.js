26c5949133b8ef64415e144f0022dc3343a49cc7
Yustynn Panicker <yustynn@gmail.com>
2016-07-31 23:06:12 +0800

    new python, adj node

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var Gpio = require('pigpio').Gpio,
   motor = new Gpio(3, {mode: Gpio.OUTPUT}),
-  pulseWidth = 5000,
-  increment = 500;
+  pulseWidth = 1000,
+  increment = 100;
 
 setInterval(function () {
   console.log('calling...')
   motor.servoWrite(pulseWidth);
 
   pulseWidth += increment;
-  if (pulseWidth >= 10000) {
-    increment = -1000;
-  } else if (pulseWidth <= 5000) {
-    increment = 1000;
+  if (pulseWidth >= 2000) {
+    increment = -100;
+  } else if (pulseWidth <= 1000) {
+    increment = 100;
   }
-}, 500);
+}, 20);
---
+++ new.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(21,GPIO.OUT)
+
+p = GPIO.PWM(21,50)        #sets pin 21 to PWM and sends 50 signals per second
+
+p.start(7.5)          #starts by sending a pulse at 7.5% to center the servo
+
+try:                      # I still don’t know what this does
+
+    while True:       #starts an infinite loop
+
+        p.ChangeDutyCycle(4.5)    #sends a 4.5% pulse to turn the servo CCW
+
+        time.sleep(0.5)                   #continues for a half a second
+
+        p.ChangeDutyCycle(10.5)    #sends a 10.5% pulse to turn the servo CW
+
+        time.sleep(0.5)                   #continues for a half a second
+
+        p.ChangeDutyCycle(7.5)    #sends a 7.5% pulse to center the servo again
+
+        time.sleep(0.5)                   #continues for a half a second
+
+except KeyboardInterrupt:
+
+    p.stop()
+
+    GPIO.cleanup()                 #supposed to stop when a key is pressed, doesn’t work
