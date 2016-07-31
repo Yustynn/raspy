93119b2721eba9065ae6de47a474792ca0c13d1d
Yustynn Panicker <yustynn@gmail.com>
2016-07-31 22:40:16 +0800

    Fix?

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var Gpio = require('pigpio').Gpio,
   motor = new Gpio(3, {mode: Gpio.OUTPUT}),
-  pulseWidth = 1000,
-  increment = 100;
+  pulseWidth = 5000,
+  increment = 500;
 
 setInterval(function () {
   console.log('calling...')
   motor.servoWrite(pulseWidth);
 
   pulseWidth += increment;
-  if (pulseWidth >= 2000) {
-    increment = -500;
-  } else if (pulseWidth <= 1000) {
-    increment = 500;
+  if (pulseWidth >= 10000) {
+    increment = -1000;
+  } else if (pulseWidth <= 5000) {
+    increment = 1000;
   }
 }, 500);
